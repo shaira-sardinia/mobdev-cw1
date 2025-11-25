444f8262e2636c385e61e06a78693711cc73916d
SSardinia <103689703+shaira-sardinia@users.noreply.github.com>
2025-11-25 01:12:19 +0000

    Delete app directory

--- app/src/main/java/org/me/gcu/sardinia_shaira_s2264713/FragmentNavigationListener.java
+++
@@ -1,6 +0,0 @@
-package org.me.gcu.sardinia_shaira_s2264713;
-
-public interface FragmentNavigationListener {
-    void onOpenConversionPage();
-    void onOpenConversionPage(String preSelectedCurrency);
-}
